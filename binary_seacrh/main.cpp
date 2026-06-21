2f0ca1e933fa672275d8c72cdc2de571a5bcaa04
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-06-21 09:50:29 +0600

    update the fix of upper bound

--- binary_seacrh/main.cpp
+++ binary_seacrh/main.cpp
@@ -8,7 +8,7 @@ int lowerBound(vector<int> v, int x)
     int ans = n;
     while (l <= r)
     {
-        int mid = (l + r) / 2;
+        int mid = l + (r - l) / 2;
         if (v[mid] >= x)
         {
             ans = mid;
@@ -27,13 +27,13 @@ int upperBound(vector<int> v, int x)
     while (l <= r)
     {
         int mid = l + (r - l) / 2;
-        if (v[mid] <= x)
+        if (v[mid] > x)
         {
-            l = mid + 1;
             ans = mid;
+            r = mid - 1;
         }
         else
-            r = mid - 1;
+            l = mid + 1;
     }
     return ans;
 }
