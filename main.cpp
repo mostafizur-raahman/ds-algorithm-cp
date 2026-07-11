8f6ed252e9705acb8424be9508c6b0a94279eefb
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-11 20:22:40 +0600

    cses problem: Two Sets

--- main.cpp
+++ main.cpp
@@ -2,28 +2,42 @@
 using namespace std;
 typedef long long ll;
 typedef vector<ll> vi;
-void solve()
-{
+
+ll sumOf1ToN(ll n) {
+    return (n * (n + 1)) / 2;
+}
+void solve() {
     ll n;
     cin >> n;
-    ll ans = INT_MAX;
-    for (int i = 0; i < n; i++)
-    {
-        ll x;
-        cin >> x;
-        if (x < 0)
-        {
-            ans = min(ans, abs(x));
-        }
-        else
-        {
-            ans = min(ans, x);
+
+    ll sum = sumOf1ToN(n);
+    if (sum % 2 != 0) {
+        cout << "NO\n";
+    }
+    ll target = sum / 2;
+    vector<int> set1, set2;
+
+    for (int i = n; i >= 1; i--) {
+        if (i <= target) {
+            set1.push_back(i);
+            target -= i;
+        } else {
+            set2.push_back(i);
         }
     }
-    cout << ans << "\n";
+    cout << "YES" << endl;
+    cout << set1.size() << endl;
+    for (int i = 0; i < set1.size(); i++) {
+        cout << set1[i] << " ";
+    }
+    cout << endl;
+    cout << set2.size() << endl;
+    for (auto u : set2) {
+        cout << u << " ";
+    }
+    cout << endl;
 }
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
