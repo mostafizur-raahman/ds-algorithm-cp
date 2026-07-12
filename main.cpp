922ee695c09c449f31f18ede5f84fbac21b977d5
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-12 17:08:39 +0600

    Static Range Sum Queries

--- main.cpp
+++ main.cpp
@@ -3,37 +3,22 @@ using namespace std;
 typedef long long ll;
 typedef vector<ll> vi;
 
-void solve() {
-    ll n;
-    cin >> n;
-    vi v;
-    bool ans = 0;
-    for (ll i = 0; i < n; i++) {
-        ll x;
-        cin >> x;
-        v.push_back(x);
-    }
-    sort(v.begin(), v.end());
-    if (v[n - 1] >= 3) {
-        cout << "YES" << endl;
-        return;
-    }
-    if (n - 2 >= 0) {
-        if (v[n - 2] >= 2) {
-            cout << "YES" << endl;
-            return;
-        }
-    }
-    cout << "NO" << endl;
-}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tc;
-    cin >> tc;
-
-    while (tc--) {
-        solve();
+    ll n, q;
+    cin >> n >> q;
+    vi v;
+    v.push_back(0);
+    for (int i = 1; i <= n; i++) {
+        ll tmp;
+        cin >> tmp;
+        v.push_back(v[i - 1] + tmp);
+    }
+    while (q--) {
+        ll l, r;
+        cin >> l >> r;
+        cout << v[r] - v[l - 1] << "\n";
     }
 }
