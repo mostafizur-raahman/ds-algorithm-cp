745cfcd37efe472187deaadda7a8ad03d2b47a17
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-13 01:22:11 +0600

    codeforces div2 A solve

--- main.cpp
+++ main.cpp
@@ -7,8 +7,39 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    vector<int> v{1, 2, 3, 4, 5};
-    for (auto it = begin(v); it != end(v); it = next(it)) {
-        cout << *it << endl;
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        ll n;
+        cin >> n;
+        vi v;
+        int cntOf2 = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            v.push_back(x);
+            if (x % 2 == 0)
+                cntOf2++;
+        }
+        if (cntOf2 % 2 == 1) {
+            cout << "-1\n";
+            continue;
+        }
+        if (cntOf2 == 0) {
+            cout << "1\n";
+            continue;
+        }
+        ll ans = cntOf2 / 2, count = 0;
+        ;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == 2) {
+                count++;
+                if (count == ans) {
+                    cout << i + 1 << "\n";
+                    continue;
+                }
+            }
+        }
     }
 }
