645b6edfd4515c257ab3e786eb8e0dd8301addb9
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-12 16:08:47 +0600

    solve cf b:

--- main.cpp
+++ main.cpp
@@ -3,43 +3,37 @@ using namespace std;
 typedef long long ll;
 typedef vector<ll> vi;
 
-ll sumOf1ToN(ll n) {
-    return (n * (n + 1)) / 2;
-}
 void solve() {
     ll n;
     cin >> n;
-
-    ll sum = sumOf1ToN(n);
-    if (sum % 2 != 0) {
-        cout << "NO\n";
+    vi v;
+    bool ans = 0;
+    for (ll i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        v.push_back(x);
     }
-    ll target = sum / 2;
-    vector<int> set1, set2;
-
-    for (int i = n; i >= 1; i--) {
-        if (i <= target) {
-            set1.push_back(i);
-            target -= i;
-        } else {
-            set2.push_back(i);
+    sort(v.begin(), v.end());
+    if (v[n - 1] >= 3) {
+        cout << "YES" << endl;
+        return;
+    }
+    if (n - 2 >= 0) {
+        if (v[n - 2] >= 2) {
+            cout << "YES" << endl;
+            return;
         }
     }
-    cout << "YES" << endl;
-    cout << set1.size() << endl;
-    for (int i = 0; i < set1.size(); i++) {
-        cout << set1[i] << " ";
-    }
-    cout << endl;
-    cout << set2.size() << endl;
-    for (auto u : set2) {
-        cout << u << " ";
-    }
-    cout << endl;
+    cout << "NO" << endl;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    solve();
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
 }
