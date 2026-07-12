b87228a79b1a06851055979e8038c2a5f95c0b73
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-13 00:10:50 +0600

    auto

--- main.cpp
+++ main.cpp
@@ -7,24 +7,8 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        ll n, q;
-        cin >> n >> q;
-        vi v(n);
-        for (int i = 0; i < n; i++) {
-            cin >> v[i];
-        }
-        vi pre(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
-            pre[i] = pre[i - 1] + v[i - 1];
-        }
-
-        while (q--) {
-            ll l, r;
-            cin >> l >> r;
-            cout << pre[r] - pre[l - 1] << "\n";
-        }
+    vector<int> v{1, 2, 3, 4, 5};
+    for (auto it = begin(v); it != end(v); it = next(it)) {
+        cout << *it << endl;
     }
 }
